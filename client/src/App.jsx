a1379789f45a741d674fcd4ccd30c84fedef058d
Rajesh97266 <rajesh97266@gmail.com>
2025-04-15 15:42:04 +0530

    project-started

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold bg-red-500 ">Hello world </h1>
+      <Button >Check</Button>
+    </>
+  );
+}
+
+export default App;
